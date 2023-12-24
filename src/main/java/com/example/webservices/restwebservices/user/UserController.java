1cede0f8587c9e39374c112b6907aeb9fef11c7a
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2023-12-24 15:29:13 +0530

    adeed post request to create a user

--- src/main/java/com/example/webservices/restwebservices/user/UserController.java
+++ src/main/java/com/example/webservices/restwebservices/user/UserController.java
@@ -1,8 +1,6 @@
 package com.example.webservices.restwebservices.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,9 @@ public class UserController {
     @GetMapping(path="users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return userDaoService.findUser(id);
+    }
+    @PostMapping(path = "/users")
+    public void createUser(@RequestBody User user) {
+        userDaoService.saveUser(user);
     }
 }
--- src/main/java/com/example/webservices/restwebservices/user/UserDaoService.java
+++ src/main/java/com/example/webservices/restwebservices/user/UserDaoService.java
@@ -9,11 +9,12 @@ import java.util.List;
 @Service
 public class UserDaoService {
     private  static List<User> users = new ArrayList<>();
+    private  static int userCount = 0;
 
     static {
-        users.add(new User(1,"Rishabh", LocalDate.now().minusYears(22)));
-        users.add(new User(2, "Tom", LocalDate.now().minusYears(40)));
-        users.add(new User(3, "Danny", LocalDate.now().minusYears(27)));
+        users.add(new User(++userCount,"Rishabh", LocalDate.now().minusYears(22)));
+        users.add(new User(++userCount, "Tom", LocalDate.now().minusYears(40)));
+        users.add(new User(++userCount, "Danny", LocalDate.now().minusYears(27)));
     }
 
     public List<User> findAllUsers() {
@@ -22,5 +23,11 @@ public class UserDaoService {
 
     public User findUser(int id) {
         return users.stream().filter(user -> user.getId() == id).findFirst().get();
+    }
+
+    public User saveUser(User user) {
+        user.setId(++userCount);
+        users.add(user);
+        return user;
     }
 }

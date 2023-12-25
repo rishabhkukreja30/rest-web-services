900606fc170310b23b09511ebb57d5b98746510f
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2023-12-25 17:48:20 +0530

    added static filtering and jsonProperty

---
+++ src/main/java/com/example/webservices/restwebservices/filtering/FilteringController.java
@@ -0,0 +1,23 @@
+package com.example.webservices.restwebservices.filtering;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class FilteringController {
+
+    @GetMapping(path = "/filtering")
+    public SomeBean getSomeBean() {
+        return new SomeBean("value1", "value2", "value3");
+    }
+
+    @GetMapping(path = "/filterings")
+    public List<SomeBean> getAllSomeBean() {
+        return Arrays.asList(new SomeBean("value1", "value2", "value3"),
+                new SomeBean("value4", "value5", "value6"));
+    }
+
+}
